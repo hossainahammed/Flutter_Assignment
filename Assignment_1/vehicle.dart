5e429854479f7ebbc24cb7ede1bb96ebb3e85432
hossainahammed <hossainahammed627@gmail.com>
2025-03-30 15:31:26 +0600

    Final Code

--- Assignment_1/vehicle.dart
+++ Assignment_1/vehicle.dart
@@ -1,6 +1,27 @@
-abstract class vehicle{
-
+abstract class Vehicle{
+ int _speed =0;
+ void move();
+  set setSpeed(int speed){
+    if (speed >= 0) {
+      _speed = speed;
+    } else {
+      print("Speed cannot be negative");
+    }
+  }
+  int get speed=>_speed;
 }
+
+class Car extends Vehicle{
+  @override
+  void move() {
+   print("The car is moving at ${speed} km/h");
+  }
+}
+
 main(){
 
+ Car BMW = Car();
+ BMW.setSpeed = 100;
+
+  BMW.move();
 }
