3b385e95557e45bda47b848020b469d253407cd1
Hossain Ahammed <hossainahammed627@gmail.com>
2025-05-17 01:03:34 +0600

    device preview give problem

--- Assignment_02/lib/main.dart
+++ Assignment_02/lib/main.dart
@@ -15,3 +15,35 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+
+// import 'package:device_preview/device_preview.dart';
+//
+// void main() {
+//   runApp(
+//     DevicePreview(
+//       enabled: true,
+//       builder: (context) => const MyApp(),
+//     ),
+//   );
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       useInheritedMediaQuery: true,
+//       locale: DevicePreview.locale(context),
+//       builder: DevicePreview.appBuilder,
+//       debugShowCheckedModeBanner: false,
+//       home: const Scaffold(
+//         body: Center(child: Text("Hello Device Preview")),
+//       ),
+//     );
+//   }
+// }
+
+
+
